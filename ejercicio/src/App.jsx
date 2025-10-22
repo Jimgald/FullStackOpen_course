99f32d5254a0ed9d526be10ebedfd4f2f1d7cef5
JaimeGDev <jaigaldel@alu.edu.gva.es>
2025-10-22 19:21:12 +0200

    2.1 completed

--- ejercicio/src/App.jsx
+++ ejercicio/src/App.jsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const Course = ({ courseCu }) => {
+  console.log(courseCu)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header courseHeader={courseCu}/>
+      <Content courseContent={courseCu}/>
+    </>
+
+  )
+
+}
+const Header = ({ courseHeader }) => {
+ 
+  return (
+    <>
+    <h1>{courseHeader.name}</h1>
+    </>
+
+  )
+
+}
+const Content = ({ courseContent }) => {
+  const parts = courseContent.parts
+  return (
+    <>
+    {parts.map(p => (
+      <Part key={p.id} coursePart={p} />
+    ))}
+    </>
+
+  )
+
+}
+const Part = ({ coursePart }) => {
+  return (
+    <>
+      <p>{coursePart.name} {coursePart.exercises}</p>
     </>
   )
+
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course courseCu={course} />
 }
 
 export default App
